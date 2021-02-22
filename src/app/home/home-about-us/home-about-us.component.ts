cd7056103c7b4a2c9d9a204abd923c538171ddc6
Kaerik Gautam <gautam.kartik05@gmail.com>
2021-02-22 21:17:29 +0530

    few improvements

--- src/app/home/home-about-us/home-about-us.component.ts
+++ src/app/home/home-about-us/home-about-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home-about-us',
@@ -10,6 +11,30 @@ export class HomeAboutUsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 400,
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 1
+      }
+    },
+    nav: false
   }
 
 }
