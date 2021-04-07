715c823198f2a2e353e7d75ff3a156adad25648b
Kaerik Gautam <gautam.kartik05@gmail.com>
2021-04-07 19:31:33 +0530

    minor fixes

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -8,7 +9,13 @@ import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog, private _activeRoute: ActivatedRoute) {
+    _activeRoute.queryParams.subscribe(res => {
+      console.log(res)
+      if (res.ref)
+        this.openDialog(res.ref);
+    })
+  }
 
   ngOnInit(): void {
   }
@@ -17,7 +24,7 @@ export class ProductsComponent implements OnInit {
     { title: 'Welding Jigs & Fixture', bg: '../../assets/products/weld_bg.png', type: 'welding', bg_size: 'cover', color: 'white' },
     { title: 'Robotics Cell & Automations', bg: '../../assets/products/robo_bg.png', type: 'robotics', bg_size: 'contain', color: 'black' },
     { title: 'SPM', bg: '../../assets/products/spm/spm.png', type: 'spm', bg_size: 'contain', color: 'black' },
-    { title: 'Fabrication & Machining', bg: '../../assets/products/line/fixtureline.png', type: 'fab', bg_size: 'cover', color: 'white' },
+    { title: 'Fabrication & Machining', bg: '../../assets/products/fabrication/fab.png', type: 'fab', bg_size: 'cover', color: 'white' },
     { title: 'Line Automation', bg: '../../assets/products/line/fixtureline.png', type: 'line', bg_size: 'cover', color: 'black' },
     { title: 'Simulation', bg: '../../assets/products/simulation/sim_bg.jpeg', type: 'sim', bg_size: 'cover', color: 'white' },
     { title: 'Automobile Components', bg: '../../assets/products/automobile/auto_bg.jpeg', type: 'auto', bg_size: 'cover', color: 'white' },
